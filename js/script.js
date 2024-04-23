fc5fe94ac0998f08670ec8bfadd472be740449da
bargo0089 <bargo0089@gmail.com>
2024-04-24 00:10:40 +0100

    update navbar

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const navbar_main = document.getElementById('navbar-main')
+addEventListener("scroll", (event) => {
+    if(window.scrollY>40){
+        navbar_main.style.backgroundColor ='white'
+        navbar_main.children[0].style.color ='black'
+    }else {
+        navbar_main.style.backgroundColor ='transparent'
+        navbar_main.children[0].style.color ='white'
+    }
+});
