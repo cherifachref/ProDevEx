06e987bb56ddadbcda72853de03cf8d0214bf14e
bargo0089 <bargo0089@gmail.com>
2024-04-24 03:28:26 +0100

    add section services

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const navbar_main = document.getElementById('navbar-main')
 addEventListener("scroll", (event) => {
     if(window.scrollY>40){
+        //navbar_main.style.display ='none'
         navbar_main.style.backgroundColor ='white'
         navbar_main.children[0].style.color ='black'
     }else {
